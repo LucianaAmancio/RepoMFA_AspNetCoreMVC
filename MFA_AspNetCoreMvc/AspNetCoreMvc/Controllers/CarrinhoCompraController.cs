cf49f565fc51577422bdd76133e96a90341a0758
LucianaAmancio <luciana.da.silva.abreu.amancio@hotmail.com>
2020-08-31 07:25:30 -0300

    init

---
+++ MFA_AspNetCoreMvc/AspNetCoreMvc/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,48 @@
+﻿using AspNetCoreMvc.Models;
+using AspNetCoreMvc.Repositories;
+using AspNetCoreMvc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+        private readonly IComidaRepository _comidaRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraController(IComidaRepository comidaRepository, CarrinhoCompra carrinhoCompra)
+        {
+            //instâncias criadas
+            _comidaRepository = comidaRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Index()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraViewModel = new CarrinhoCompraViewModel();
+            var CarrinhoCompra = _carrinhoCompra;
+            var CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal();
+           
+            return View(carrinhoCompraViewModel);
+         }
+
+        public RedirectToActionResult AdicionarItemNoCarrinhoCompra(int comidaId)
+        {
+            var comidaSelecionada = _comidaRepository.Comidas.FirstOrDefault(p => p.ComidaId == comidaId);
+
+            if (comidaSelecionada != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(comidaSelecionada, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }  
+    
+}
+
---
+++ MFA_AspNetCoreMvc/AspNetCoreMvc/ViewModels/CarrinhoCompraViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreMvc.Models;
+
+namespace AspNetCoreMvc.ViewModels
+{
+    public class CarrinhoCompraViewModel
+    {
+
+        //Será o que você quer exibir na View
+
+        public CarrinhoCompra CarrinhoCompra { get; set; }
+        public decimal CarrinhoCompraTotal { get; set; }
+
+
+    }
+}
